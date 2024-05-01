bf74ec148a9ee2201aa79cc1b67b6b8726c67648
Abiodun Bankole <abbey.developer@gmail.com>
2024-05-01 12:55:45 +0100

    bugs

--- components/about.tsx
+++ components/about.tsx
@@ -5,6 +5,7 @@ import Container from "./container";
 import aboutImg from "@/public/images/image-about.png";
 import vector from "@/public/images/vector.png";
 import { Button } from "./ui/button";
+import { Mail } from "lucide-react";
 
 const About = () => {
   return (
@@ -24,7 +25,7 @@ const About = () => {
 
       <div className="flex flex-col lg:flex-row justify-between items-center gap-4">
         <div>
-          <p className="w-full lg:w-1/2 text-xl font-normal border-l-4 border-black dark:border-white px-6 mt-5">
+          <p className="w-full lg:w-[70%] text-xl font-normal border-l-4 border-black dark:border-white px-6 mt-5">
             I am a software engineer with 5+ years of experience in the tech
             industry. I am passionate about building innovative and
             user-friendly software solutions. I have a strong understanding of
@@ -35,11 +36,8 @@ const About = () => {
             and opportunities to learn and grow.
           </p>
           <a href="https://wa.me/2347017181619">
-            <Button
-              className="bg-green-400 rounded-full text-white mt-10"
-              variant="default"
-            >
-              Send me a WhatsApp message
+            <Button className="rounded-full mt-10" variant="default">
+              <Mail className="h-4 w-4 mr-2" /> Send a mail
             </Button>
           </a>
         </div>
